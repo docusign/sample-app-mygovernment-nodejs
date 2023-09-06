a28abd373ac927bf5eaddf331af8de4b4178b3a9
rashman844 <100536181+rashman844@users.noreply.github.com>
2023-09-06 18:20:06 -0400

    new

--- server/routes/passportRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const passportController = require('../controllers/passportController');
-
-router.post('/', passportController.createController);
-
-module.exports = router;
